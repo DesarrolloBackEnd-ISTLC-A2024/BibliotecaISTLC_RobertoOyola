c1324c077a85e9a04c1da5bec5cb0a65410b9e70
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2024-08-07 20:17:44 -0500

    a

---
+++ BibliotecaISTLC/Models/Libro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BibliotecaISTLC.Models;
+
+public partial class Libro
+{
+    public int IdLibros { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public int? IdCategoria { get; set; }
+
+    public int? IdAutor { get; set; }
+
+    public int? IdEditorial { get; set; }
+
+    public string? Estado { get; set; }
+
+    public virtual Autor? IdAutorNavigation { get; set; }
+
+    public virtual Categoria? IdCategoriaNavigation { get; set; }
+
+    public virtual Editorial? IdEditorialNavigation { get; set; }
+
+    public virtual Inventario? Inventario { get; set; }
+
+    public virtual ICollection<Movimiento> Movimientos { get; set; } = new List<Movimiento>();
+
+    public virtual ICollection<PrestamoDetalle> PrestamoDetalles { get; set; } = new List<PrestamoDetalle>();
+}
